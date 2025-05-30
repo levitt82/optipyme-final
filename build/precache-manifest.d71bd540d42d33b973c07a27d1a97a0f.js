8d1efe020c0cae3d78cd5f89e1fbb0eeec102d10
levitt82 <optipymesolutions@gmail.com>
2025-05-30 14:35:32 +0200

    Force add build folder

---
+++ build/precache-manifest.d71bd540d42d33b973c07a27d1a97a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55b47b39ac2c7ba1dbe96030d0fdd9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04574881253066be402a",
+    "url": "/static/css/main.cd5b9348.chunk.css"
+  },
+  {
+    "revision": "8c73a099bd930610d7f4",
+    "url": "/static/js/2.2eb7d949.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "04574881253066be402a",
+    "url": "/static/js/main.905973bf.chunk.js"
+  },
+  {
+    "revision": "d9255067eef9f997e00f",
+    "url": "/static/js/runtime~main.f8e697fc.js"
+  }
+]);
